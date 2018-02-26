a6ec7c8860553f0261b48ce51cac740883b217e3
Jerome <chery.jerome@online.fr>
2018-02-26 19:49:38 +0100

    text2Speech

--- test.js
+++ test.js
@@ -1,15 +1,19 @@
-var lambda = require('./index.js');
-var fs = require('fs');
+"use strict";
 
-var test = function (state,res) {
-    if (state) { console.log(state); }
+var lambda = require("./index.js");
+var fs = require("fs");
+
+var test = function (state, res) {
+    if (state) {
+        console.log(state);
+    }
     console.log(res);
-}
+};
 
-fs.readFile('./test.html', function read(err, html) {
+fs.readFile("./test.html", function read(err, html) {
     if (err) {
         throw err;
     }
-    // lambda.handler({html}, null, test);
-    console.log({html:html.toString()});
+    lambda.handler({html}, null, test);
+    // console.log({html:html.toString()});
 });
