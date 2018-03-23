2a55c3980421df582e5ebcd9b18656912a7aebfb
Jerome Chery <jerome.chery@nrco.fr>
2018-03-23 16:43:38 +0100

    indentification des erreurs

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ exports.handler = (event, context, callback) => {
   if ((event.body) && (!event.html)) {
     event = JSON.parse(event.body);
   }
-  return Promise.resolve(event)
-    .then(obj => P.cleanHtml(obj))
-    .then(obj => P.mkMD5(obj))
-    .then(obj => P.checkInBucket(obj))
-    .then(
-      obj => obj.found
-          ? obj
-          : Promise.resolve(obj)
-              .then(obj => P.textToSSMLSpeech(obj))
-              .then(obj => P.saveToBucket(obj))
-    )
-    .then(obj => {
-      obj.success = true;
-      return callback(null, obj)
-    })
-    .catch(err => callback({success:false, err}));
+  return Promise.resolve(event).then(obj =>
+    Promise.resolve(obj)
+      .then(obj => P.cleanHtml(obj))
+      .then(obj => P.mkMD5(obj))
+      .then(obj => P.checkInBucket(obj))
+      .then(
+        obj =>
+          obj.found
+            ? obj
+            : Promise.resolve(obj)
+                .then(obj => P.textToSSMLSpeech(obj))
+                .then(obj => P.saveToBucket(obj))
+      )
+      .then(obj => {
+        obj.success = true;
+        return callback(null, obj);
+      })
+      .catch(err => callback(JSON.stringify({ obj, err })))
+  );
 };
